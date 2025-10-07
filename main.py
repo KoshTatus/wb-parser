7a7ddca7c7a35a65131ed1fce117d698d244c239
KoshTatus <153766064+KoshTatus@users.noreply.github.com>
2025-10-07 11:45:09 +0400

    add entry point

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import uvicorn
 from fastapi import FastAPI, Query
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
-from src.app.dao import ProductDAO
-from src.database.database import get_db
+from src.dao.dao import ProductDAO
+from src.database.database import get_db, create_tables
+from src.parser.parser import parse_products_from_wb
 
 app = FastAPI()
 
-
 @app.get("/products")
-def get_products(title = Query(), db: Session = Depends(get_db)):
+def get_products(title = Query(default=""), db: Session = Depends(get_db)):
     result = ProductDAO.get_all_filtered(db, title)
 
     return {
@@ -24,3 +25,17 @@ def get_products(title = Query(), db: Session = Depends(get_db)):
             for product in result
         ]
     }
+
+@app.post("/products")
+def parse_products(query = Query(default="термопаста"), db: Session = Depends(get_db)):
+    result = parse_products_from_wb(query=query)
+
+    ProductDAO.add_products(db, result)
+
+    return {
+        "status": "OK",
+    }
+
+if __name__ == "__main__":
+    create_tables()
+    uvicorn.run(app, host="localhost", port=5000)
